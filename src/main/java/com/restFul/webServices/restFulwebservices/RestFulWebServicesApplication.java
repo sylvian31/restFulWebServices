8c37a482900132fa709b2b173feb2ccdeb6da6eb
sylvian <sylvian31@gmail.com>
2019-04-06 11:01:59 -0400

    First commit

---
+++ src/main/java/com/restFul/webServices/restFulwebservices/RestFulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.restFul.webServices.restFulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulWebServicesApplication.class, args);
+	}
+
+}
